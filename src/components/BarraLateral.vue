22f7dd4880c09f0483e82e3a06bffc97346718b3
Emanuel Angelo <emanuelangelo@outlook.com.br>
2023-03-27 14:17:42 -0300

    inciando formacao

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <header>
+      <h1>
+        <img src="../assets/logo.png" alt="" srcset="" />
+      </h1>
+    </header>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BarraLateral",
+});
+</script>
+
+<style scoped>
+header {
+  padding: 1.5rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+}
+
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
